a375835bf9a32d25107c848abf0da7c829bf16d8
yakuboff-m <m.yakuboff.15@gmail.com>
2025-11-06 16:14:54 +0900

    fix: MUI customized integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import '../css/app.css';
-import { Box, Button, Typography, Stack, Container} from "@mui/material";
+import { Box, Button, Typography, Stack, Container, } from "@mui/material";
+import { RippleBadge } from './MaterialTheme/styled';
 
 function App() {
   return (
-    <Container maxWidth="sm">
+    <Container sx={{background: "orange"}}>
       <Stack flexDirection={"column"}>
         <Box sx={{my: 4}}>
           <Typography variant="h4" component={"h4"}>
             Create React App on Typescript with REDUX
           </Typography>
         </Box>
-        <Button variant="contained">Contained</Button>
+        <Box>
+          <RippleBadge badgeContent={4}>
+            <Button variant="contained">Contained</Button>
+          </RippleBadge>
+        </Box>
       </Stack>
     </Container>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "./theme";
 import './css/index.css';
+import theme from './app/MaterialTheme';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
