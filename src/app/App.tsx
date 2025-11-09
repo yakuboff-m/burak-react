fbc0c81b07fc821c781013a60b291e247c75f76e
yakuboff-m <m.yakuboff.15@gmail.com>
2025-11-09 20:25:59 +0900

    feat: create routing system

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,46 @@
 import React from 'react';
 import '../css/app.css';
 import { Box, Button, Typography, Stack, Container, } from "@mui/material";
-import { RippleBadge } from './MaterialTheme/styled';
+import { Link, Route, Router, Switch } from 'react-router-dom';
+import { About } from './screens/About';
+import { Users } from './screens/Users';
 
 function App() {
   return (
-    <Container sx={{background: "orange"}}>
-      <Stack flexDirection={"column"}>
-        <Box sx={{my: 4}}>
-          <Typography variant="h4" component={"h4"}>
-            Create React App on Typescript with REDUX
-          </Typography>
-        </Box>
-        <Box>
-          <RippleBadge badgeContent={4}>
-            <Button variant="contained">Contained</Button>
-          </RippleBadge>
-        </Box>
-      </Stack>
-    </Container>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './app/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./app/App";
+import reportWebVitals from "./reportWebVitals";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
-import './css/index.css';
-import theme from './app/MaterialTheme';
+import "./css/index.css";
+import theme from "./app/MaterialTheme";
+import { BrowserRouter as Router } from "react-router-dom";
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
@@ -17,11 +18,12 @@ root.render(
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <App />
+        <Router>
+          <App />
+        </Router>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>
-  
 );
 
 // If you want to start measuring performance in your app, pass a function
