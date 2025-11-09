42c82495d94ab8ff52e27677fbcde833dd21daf0
yakuboff-m <m.yakuboff.15@gmail.com>
2025-11-09 20:50:53 +0900

    fix: develop burak screen based components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import '../css/app.css';
 import { Box, Button, Typography, Stack, Container, } from "@mui/material";
 import { Link, Route, Router, Switch } from 'react-router-dom';
-import { About } from './screens/About';
-import { Users } from './screens/Users';
+import { HomePage } from './screens/homePage';
+import { ProductsPage } from './screens/productsPage/index';
+import { OrdersPage } from './screens/ordersPage/index';
+import { UserPage } from './screens/userPage';
 
 function App() {
   return (
@@ -11,13 +13,16 @@ function App() {
         <nav>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/">HomePage</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/products">ProductsPage</Link>
             </li>
             <li>
-              <Link to="/users">Users</Link>
+              <Link to="/orders">OrdersPage</Link>
+            </li>
+            <li>
+              <Link to="/member-page">UserPage</Link>
             </li>
           </ul>
         </nav>
@@ -25,22 +30,22 @@ function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <About />
+          <Route path="/products">
+            <ProductsPage />
           </Route>
-          <Route path="/users">
-            <Users />
+          <Route path="/orders">
+            <OrdersPage />
+          </Route>
+          <Route path="/member-page">
+            <UserPage />
           </Route>
           <Route path="/">
-            <Home />
+            <HomePage />
           </Route>
         </Switch>
       </div>
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
 
 export default App;
