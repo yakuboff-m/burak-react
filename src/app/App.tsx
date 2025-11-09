5e6c0268488908cdc325ccc35c02adcc6e2d879f
yakuboff-m <m.yakuboff.15@gmail.com>
2025-11-10 06:45:11 +0900

    fix: organize header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,51 +1,41 @@
-import React from 'react';
-import '../css/app.css';
-import { Box, Button, Typography, Stack, Container, } from "@mui/material";
-import { Link, Route, Router, Switch } from 'react-router-dom';
-import { HomePage } from './screens/homePage';
-import { ProductsPage } from './screens/productsPage/index';
-import { OrdersPage } from './screens/ordersPage/index';
-import { UserPage } from './screens/userPage';
+import React from "react";
+import "../css/app.css";
+import { Box, Button, Typography, Stack, Container } from "@mui/material";
+import { Link, Route, Router, Switch, useLocation } from "react-router-dom";
+import { HomePage } from "./screens/homePage";
+import { ProductsPage } from "./screens/productsPage/index";
+import { OrdersPage } from "./screens/ordersPage/index";
+import { UserPage } from "./screens/userPage";
+import { OtherNavbar } from "./components/headers/OtherNavbar";
+import { HomeNavbar } from "./components/headers/HomeNavbar";
+import { Footer } from "./components/footer";
 
 function App() {
-  return (
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">HomePage</Link>
-            </li>
-            <li>
-              <Link to="/products">ProductsPage</Link>
-            </li>
-            <li>
-              <Link to="/orders">OrdersPage</Link>
-            </li>
-            <li>
-              <Link to="/member-page">UserPage</Link>
-            </li>
-          </ul>
-        </nav>
+  const location = useLocation(); // returns Object
+  console.log("location:", location);
 
-        {/* A <Switch> looks through its children <Route>s and
+  return (
+    <>
+      {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/products">
-            <ProductsPage />
-          </Route>
-          <Route path="/orders">
-            <OrdersPage />
-          </Route>
-          <Route path="/member-page">
-            <UserPage />
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </div>
+      {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
+      <Switch>
+        <Route path="/products">
+          <ProductsPage />
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <UserPage />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+      <Footer />
+    </>
   );
 }
-
 
 export default App;
