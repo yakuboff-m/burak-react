c30f2cec2a121f5ee71e98c67e7ee4d54c29ea4a
yakuboff-m <m.yakuboff.15@gmail.com>
2025-11-12 20:55:50 +0900

    fix: adding temporary minHeight to screen component -> wiping after-footer white space

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Box, Button, Typography, Stack, Container } from "@mui/material";
 import { Link, Route, Router, Switch, useLocation } from "react-router-dom";
-import  HomePage  from "./screens/homePage";
-import  ProductsPage  from "./screens/productsPage/index";
-import  OrdersPage  from "./screens/ordersPage/index";
-import  UserPage  from "./screens/userPage";
-import  OtherNavbar  from "./components/headers/OtherNavbar";
-import  HomeNavbar  from "./components/headers/HomeNavbar";
-import  Footer  from "./components/footer";
+import HomePage from "./screens/homePage";
+import ProductsPage from "./screens/productsPage/index";
+import OrdersPage from "./screens/ordersPage/index";
+import UserPage from "./screens/userPage";
+import OtherNavbar from "./components/headers/OtherNavbar";
+import HomeNavbar from "./components/headers/HomeNavbar";
+import Footer from "./components/footer";
 import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css";
@@ -22,23 +22,26 @@ function App() {
       {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
       {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
-      <Switch>
-        <Route path="/products">
-          <ProductsPage />
-        </Route>
-        <Route path="/orders">
-          <OrdersPage />
-        </Route>
-        <Route path="/member-page">
-          <UserPage />
-        </Route>
-        <Route path="/help">
-          <HelpPage />
-        </Route>
-        <Route path="/">
-          <HomePage />
-        </Route>
-      </Switch>
+      {/* temporary minHeight */}
+      <Box sx={{ minHeight: "20vh" }}>
+        <Switch>
+          <Route path="/products">
+            <ProductsPage />
+          </Route>
+          <Route path="/orders">
+            <OrdersPage />
+          </Route>
+          <Route path="/member-page">
+            <UserPage />
+          </Route>
+          <Route path="/help">
+            <HelpPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Box>
       <Footer />
     </>
   );
