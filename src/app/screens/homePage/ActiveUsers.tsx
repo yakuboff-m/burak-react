b049ccb4ac39accd365cd6e75a912913eb902878
yakuboff-m <m.yakuboff.15@gmail.com>
2025-11-16 08:38:43 +0900

    feat: develop activeUsers sectional component

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -1,10 +1,44 @@
-import React from "react";
-import { Container } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
+import Card from "@mui/joy/Card";
+import { CssVarsProvider, Typography } from "@mui/joy";
+import CardOverflow from "@mui/joy/CardOverflow";
+import AspectRatio from "@mui/joy/AspectRatio";
+
+const activeUsers = [
+  { memberNick: "Martin", memberImage: "/img/martin.webp" },
+  { memberNick: "Justin", memberImage: "/img/justin.webp" },
+  { memberNick: "Rose", memberImage: "/img/rose.webp" },
+  { memberNick: "Nusret", memberImage: "/img/nusret.webp" },
+];
 
 export default function ActiveUsers() {
-    return (
-        <div>
-            <Container>ActiveUsers</Container>
-        </div>
-    )
+  return (
+    <div className={"active-users-frame"}>
+      <Container>
+        <Stack className={"main"}>
+          <Box className={"category-title"}>Active Users</Box>
+          <Stack className={"cards-frame"}>
+            <CssVarsProvider>
+              {activeUsers.length !== 0 ? (
+                activeUsers.map((ele, index) => {
+                    return (
+                        <Card key={index} className="active-user-card">
+                            <Box className="active-user-img">
+                                <img src={ele.memberImage}  alt="" />
+                            </Box>
+                            <Typography className="active-user-nick">
+                                {ele.memberNick}
+                            </Typography>
+                        </Card>
+                    )
+                })
+              ) : (
+                <Box className="no-data">No Active Users!</Box>
+              )}
+            </CssVarsProvider>
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
